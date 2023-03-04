3420daa4a6d13897fee5225cf3c3730067b882c1
RJC <rauljarodconanan@gmail.com>
2023-03-04 17:40:49 +0800

    forum app url endpoints added

--- widget_robo_mommy/widget_robo_mommy/settings.py
+++ widget_robo_mommy/widget_robo_mommy/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'forum.apps.ForumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
