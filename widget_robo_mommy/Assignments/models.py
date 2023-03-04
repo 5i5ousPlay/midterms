207e7450af6e3f5a40e73ada416820422e2e567e
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-03-05 01:27:24 +0800

    Fixed model relations

--- widget_robo_mommy/Assignments/models.py
+++ widget_robo_mommy/Assignments/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Course(models.Model):
+    code = models.CharField(max_length=10, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    section = models.CharField(max_length=3, blank=True, null=True)
+
 class Assignment(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
@@ -10,8 +15,3 @@ class Assignment(models.Model):
     def save(self, *args, **kwargs):
         self.passing_score = int(self.perfect_score * 0.60)
         super(Assignment, self).save(*args, **kwargs)
-
-class Course(models.Model):
-    code = models.CharField(max_length=10, blank=True, null=True)
-    title = models.CharField(max_length=255, blank=True, null=True)
-    section = models.CharField(max_length=3, blank=True, null=True)
