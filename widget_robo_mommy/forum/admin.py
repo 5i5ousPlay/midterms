6ea4faf3108b1c3ee8fed4835196b41228781e16
RJC <rauljarodconanan@gmail.com>
2023-03-04 17:41:13 +0800

    registered models to admin panel

--- widget_robo_mommy/forum/admin.py
+++ widget_robo_mommy/forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ForumPost, Reply, WidgetUser
 
 # Register your models here.
+admin.site.register(ForumPost)
+admin.site.register(Reply)
+admin.site.register(WidgetUser)
