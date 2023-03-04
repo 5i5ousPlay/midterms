08083653c7d4e98ad7af11d57d1271fb66faba18
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-03-05 01:18:59 +0800

    Set up the admin panel for Assignments

--- widget_robo_mommy/Assignments/admin.py
+++ widget_robo_mommy/Assignments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Subject, IndexCard
 
-# Register your models here.
+class AssignmentAdmin(admin.ModelAdmin):
+    model = Assignment
+
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+
+admin.site.register(Assignment, AssignmentAdmin)
+admin.site.register(Course, CourseAdmin)
