f00bcb9e18e8c555dac2522b0e41f117b1e7d654
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-03-05 00:57:08 +0800

    Created the view function for Assignments

--- widget_robo_mommy/Assignments/views.py
+++ widget_robo_mommy/Assignments/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Assignment, Course
 
-# Create your views here.
+
+def index(request):
+    output = f"Widget's Assignments Page\n"
+    count = Assignment.objects.all().count()
+
+    for i in range(1, count + 1):
+        assignments = Assignment.objects.get(id=i)
+        courses = Course.objects.get(id=i)
+        output += (f"Assignment Name: {assignments.mame}\n"
+                   f"Description: {assignments.description}\n"
+                   f"Perfect Score: {assignments.perfect_score}\n"
+                   f"Passing Score: {assignments.passing_score}\n"
+                   f"Course/Section: {courses.code} {courses.title}-{courses.section}\n\n")
+
+    return HttpResponse(output)
