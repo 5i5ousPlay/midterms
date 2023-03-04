457ef1a262b3f42abdd504fcf6dbd0c13989e08a
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-03-04 23:24:08 +0800

    Added two models: Assignment and Course

--- widget_robo_mommy/Assignments/models.py
+++ widget_robo_mommy/Assignments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Assignment(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    course = models.CharField(max_length=255, blank=True, null=True)
+    perfect_score = models.IntegerField(blank=True, null=True)
+    passing_score = models.IntegerField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.passing_score = int(self.perfect_score * 0.60)
+        super(Assignment, self).save(*args, **kwargs)
+
+class Course(models.Model):
+    code = models.CharField(max_length=10, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    section = models.CharField(max_length=3, blank=True, null=True)
