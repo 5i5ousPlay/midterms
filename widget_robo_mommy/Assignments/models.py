f74a47a4d9f017b4c1c6ce4717f1cd4d484334ca
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-03-05 01:26:00 +0800

    Fixed model relations

--- widget_robo_mommy/Assignments/models.py
+++ widget_robo_mommy/Assignments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Assignment(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    course = models.CharField(max_length=255, blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     perfect_score = models.IntegerField(blank=True, null=True)
     passing_score = models.IntegerField(blank=True, null=True)
 
