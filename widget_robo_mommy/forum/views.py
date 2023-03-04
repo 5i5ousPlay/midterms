1865336073b073c1ddadd5bf11b4c1b8b6f100e6
RJC <rauljarodconanan@gmail.com>
2023-03-04 17:41:00 +0800

    forum app views added

--- widget_robo_mommy/forum/views.py
+++ widget_robo_mommy/forum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import ForumPost, Reply
 
 # Create your views here.
+class ForumPostListView(generic.ListView):
+    model = ForumPost
+    template_name = 'forum/forumpost_list.html'
