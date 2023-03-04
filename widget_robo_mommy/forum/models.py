dfa9afb007bd9325507826810a2f425782a55286
RJC <rauljarodconanan@gmail.com>
2023-03-04 23:11:20 +0800

    modified models.py

--- widget_robo_mommy/forum/models.py
+++ widget_robo_mommy/forum/models.py
@@ -13,7 +13,7 @@ class ForumPost(models.Model):
     pub_datetime = models.DateTimeField(auto_now_add=True)
 
 class Reply(models.Model):
+    forumpost = models.ForeignKey(ForumPost, related_name='reply', on_delete=models.CASCADE, null=True)
     body = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(WidgetUser, related_name='reply', on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(WidgetUser, on_delete=models.CASCADE, null=True)
     pub_datetime = models.DateTimeField(auto_now_add=True)
-
