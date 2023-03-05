efd8c1985db47c2587ec5b6e267d17c0c1470878
Al.Vanilla <al.bomediano@obf.ateneo.edu>
2023-03-05 20:52:15 +0800

    Add app Calendar with development prerequisites

--- widget_robo_mommy/widget_robo_mommy/urls.py
+++ widget_robo_mommy/widget_robo_mommy/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('widget_Calendar/', include('widget_Calendar.urls', namespace="widget_Calendar")),
     path('admin/', admin.site.urls),
 ]
