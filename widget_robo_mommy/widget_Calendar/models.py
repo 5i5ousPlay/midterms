02dce7276918870a4c713234bf7af13df924dac5
Al.Vanilla <al.bomediano@obf.ateneo.edu>
2023-03-06 00:25:19 +0800

    Add models Location and Event

--- widget_robo_mommy/widget_Calendar/models.py
+++ widget_robo_mommy/widget_Calendar/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    MODE_TYPES = (
+        ('onsite', 'Onsite'),
+        ('online', 'Online'),
+        ('hybrid', 'Hybrid'),
+    )
+    mode = models.CharField(max_length=6, choices=MODE_TYPES)
+    venue = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Mode: {} || Venue: {}'.format(self.mode, self.venue)
+
+
+class Event(models.Model):
+    target_datetime = models.CharField(max_length=20)
+    activity = models.CharField(max_length=100)
+    estimated_hours = models.FloatField()
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE
+    )
+    course = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '{} on {}'.format(self.activity, self.target_datetime)
