3fbd0a3a688e052e7e51172bd80b8083b1056eed
Al.Vanilla <al.bomediano@obf.ateneo.edu>
2023-03-06 01:43:10 +0800

    Add simple html ui for calendar activities

--- widget_robo_mommy/widget_Calendar/views.py
+++ widget_robo_mommy/widget_Calendar/views.py
@@ -1,5 +1,24 @@
+from .models import Event, Location
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World! This came from the index view')
+    html_string = 'robo_mommy’s Calendar of Activities<br>'
+    for eventItem in Event.objects.all():
+        html_string += '''
+            <br>
+            Date and Time: {}<br>
+            Activity: {}<br>
+            Estimated Hours: {}<br>
+            Course/Section: {}<br>
+            Mode: {}<br>
+            Venue: {}<br><br>
+        '''.format(
+            eventItem.target_datetime,
+            eventItem.activity,
+            eventItem.estimated_hours,
+            eventItem.course,
+            eventItem.location.mode,
+            eventItem.location.venue,
+        )
+    return HttpResponse(html_string)
