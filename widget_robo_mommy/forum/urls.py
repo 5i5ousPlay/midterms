ef23dbae2459509aa69e4eb3d5f7336fee196f10
RJC <rauljarodconanan@gmail.com>
2023-03-05 11:07:12 +0800

    changed to accommodate function based view

--- widget_robo_mommy/forum/urls.py
+++ widget_robo_mommy/forum/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('forum/', ForumPostListView.as_view())
+    path('forum/', forum_post_list_view, name='forum_post_list_view')
 ]
