39adbf2998c6d75f2db0f4c95ca5ab6606e6d920
RJC <rauljarodconanan@gmail.com>
2023-03-05 11:07:31 +0800

    change to accommodate merged Dashboard app

--- widget_robo_mommy/widget_robo_mommy/urls.py
+++ widget_robo_mommy/widget_robo_mommy/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('forum.urls', 'forum'), namespace='forum')),
+    path('Dashboard/', include('Dashboard.urls', namespace="Dashboard")),
 ]
