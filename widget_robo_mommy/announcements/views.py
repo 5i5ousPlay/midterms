7c265b3a54ef00ad8167e66099ce385413a1486b
Jiuvi Anne Hu <jiuvi.hu@obf.ateneo.edu>
2023-03-06 13:22:51 +0800

    Update views.py

--- widget_robo_mommy/announcements/views.py
+++ widget_robo_mommy/announcements/views.py
@@ -19,12 +19,12 @@ def index(request):
     
     html_string_2 = ""
     for announced in Announcement.objects.all():
-        html_string_2 += "{} by {} {} published {}<br />:\
+        html_string_2 += "{} by {} {} published {}:\
                         {}".format(announced.title, announced.author.first_name,
                         announced.author.last_name,
                         convert_to_localtime(announced.pub_datetime), announced.body)
         for reacts in announced.reaction.all():
-            html_string_2 += "{}: {}".format(reacts.name, reacts.tally)
+            html_string_2 += "{}: {}<br />".format(reacts.name, reacts.tally)
     
     html_string_final = html_string_1 + html_string_2 + "</html>"
     
