016bb91f7f42be94009567f081f87fed2ffe391b
RJC <rauljarodconanan@gmail.com>
2023-05-11 23:46:41 +0800

    added url endpoint for forumpost detail view

--- widget_robo_mommy/forum/urls.py
+++ widget_robo_mommy/forum/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('forum/', forum_post_list_view, name='forum_post_list_view')
+    path('forum/', forum_post_list_view, name='forum_post_list_view'),
+    path('forum/forumposts/<int:pk>/details/', ForumPostDetailView.as_view(), name='forumpostdetailview')
 ]
