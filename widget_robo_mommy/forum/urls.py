ac543a51ee221bb7d991a781a4f25f650c84acf6
RJC <rauljarodconanan@gmail.com>
2023-05-12 00:22:59 +0800

    added url endpoint for forumpostupdateview

--- widget_robo_mommy/forum/urls.py
+++ widget_robo_mommy/forum/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('forum/', forum_post_list_view, name='forum_post_list_view'),
     path('forum/forumposts/<int:pk>/details/', ForumPostDetailView.as_view(), name='forumpostdetailview'),
     path('forum/forumposts/add/', ForumPostCreateView.as_view(), name='forumpostcreateview'),
+    path('forum/forumposts/<int:pk>/edit/', ForumPostUpdateView.as_view(), name='forumpostupdateview'),
 ]
 
 app_name = 'forum'
