1427b7380867cd9299d6a4a142adbe219c85d21f
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-05-11 23:58:26 +0800

    Imported the new ModelForms and created class-based views to create or update data

--- widget_robo_mommy/Assignments/views.py
+++ widget_robo_mommy/Assignments/views.py
@@ -1,9 +1,33 @@
 from django.http import HttpResponse
-from .models import Assignment
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
+from .models import Assignment, Course
+from .forms import AssignmentForm, CourseForm
+
+
+class AssignmentDetailsView(DetailView):
+    model = Assignment
+    template_name = 'assignments/assignment-details.html'
+
+
+class AddAssignmentView(CreateView):
+    model = Assignment
+    form_class = AssignmentForm
+    template_name = 'assignments/assignment-add.html'
+
+
+class EditAssignmentView(UpdateView):
+    model = Assignment
+    form_class = AssignmentForm
+    template_name = 'assignments/assignment-edit.html'
 
 
 def index(request):
-    output = f"Widget's Assignments Page<br><br>"
+    output = f"Widget's assignments Page<br><br>"
     count = Assignment.objects.all().count()
 
     for i in range(1, count + 1):
