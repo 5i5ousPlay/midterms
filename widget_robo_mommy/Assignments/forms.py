512987e63435760e2803636a09157de2fe689639
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-05-12 20:50:55 +0800

    Removed ModelForm for Course and changed labels and updated widgets for the Assignment ModelForm

--- widget_robo_mommy/Assignments/forms.py
+++ widget_robo_mommy/Assignments/forms.py
@@ -1,14 +1,24 @@
 from django.forms import ModelForm
-from .models import Course, Assignment
-
-
-class CourseForm(ModelForm):
-    class Meta:
-        model = Course
-        fields = "__all__"
+from .models import Assignment
+from django import forms
 
 
 class AssignmentForm(ModelForm):
     class Meta:
         model = Assignment
         fields = "__all__"
+
+        labels = {
+            'name': '',
+            'description': '',
+            'course': 'Course: ',
+            'perfect_score': '',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Assignment Name'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+            'course': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Course'}),
+            'perfect_score': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Perfect Score'}),
+            'passing_score': forms.HiddenInput(),
+        }
