e7848cacaaa94d45b971374e6a5658bd949c86d2
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-05-12 20:52:01 +0800

    Update urls.py
    Fixed a typo

--- widget_robo_mommy/Assignments/urls.py
+++ widget_robo_mommy/Assignments/urls.py
@@ -6,7 +6,7 @@ from .views import (
 
 urlpatterns = [
     path('', index, name='index'),
-    path('add/', AddAssignmentView.as_view(), name='assignments=add'),
+    path('add/', AddAssignmentView.as_view(), name='assignments-add'),
     path('<int:pk>/edit/', EditAssignmentView.as_view(), name='assignments-edit'),
     path('<int:pk>/details/', AssignmentDetailsView.as_view(), name='assignments-item'),
 ]
