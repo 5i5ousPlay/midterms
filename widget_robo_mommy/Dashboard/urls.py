dbf1408f2c737623ff1838ca99d66a9b5fbf4312
Lance Santuyo <lance.santuyo@obf.ateneo.edu>
2023-05-14 16:07:28 +0800

    Updated Dashboard URLs

--- widget_robo_mommy/Dashboard/urls.py
+++ widget_robo_mommy/Dashboard/urls.py
@@ -4,12 +4,9 @@ from .views import (Dashboard_list_view, WidgetUserDetailView,
 
 urlpatterns = [
     path('dashboard/', Dashboard_list_view, name='Dashboard_list_view'),
-    path('widgetusers', WidgetUserDetailView.as_view(),
-         name='widgetuser-detail'),
-    path('widgetusers', WidgetUserAddView.as_view(),
-         name='widgetuser-add'),
-    path('widgetusers/<int:pk>/edit/', WidgetUserUpdateView.as_view(),
-         name='widgetuser-edit')
+    path('widgetusers/<int:pk>/details/', WidgetUserDetailView.as_view(), name='widgetuser-detail'),
+    path('widgetusers/add/', WidgetUserAddView.as_view(), name='widgetuser-add'),
+    path('widgetusers/<int:pk>/edit/', WidgetUserUpdateView.as_view(), name='widgetuser-edit')
 ]
 
 app_name = "Dashboard"
